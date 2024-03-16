c16c4d9d445bde637a589453bcf6e103909c6a59
Furkan Filiz <furkanfiliz>
2024-03-16 23:30:16 +0300

    Initial web site publish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,63 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./page.scss";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="main">
+      <div className="information">
+        <div className="image">
+          <Image
+            src="/furkanfiliz.jpg"
+            alt="Furkan Filiz"
+            width={100}
+            height={100}
+            priority
+          />
+        </div>
+        <div className="title">
+          <span>Merhaba Ben Furkan Filiz</span>
+          <span>Frontend Developer </span>
+        </div>
+        <div className="desciprition">
+          <p>8 yıldır frontend developer olarak çalışıyorum. Html, Css, Javascript ve React ile çalışıyorum.  </p>
+          <p>2010 yılında lisede başladığım yazılım serüvenine yeni heyacanlar katarak devam ediyorum. </p>
+        </div>
+        <div className="social">
+          <span>
+            <Link href="    https://github.com/furkanfiliz" target="_blank">
+
+              <Image
+                src="/social/github.png"
+                alt="Furkan Filiz || codepen"
+                width={100}
+                height={100}
+                priority
+              />
+            </Link>
+            <Link href="https://codepen.io/furkanfiliz" target="_blank">
+              <Image
+                src="/social/codepen.svg"
+                alt="Furkan Filiz || codepen"
+                width={100}
+                height={100}
+                priority
+              />
+            </Link>
+
+            <Link href="https://tr.linkedin.com/in/furkan-filiz-181b12125" target="_blank">
+              <Image
+                src="/social/linkedin.png"
+                alt="Furkan Filiz || codepen"
+                width={100}
+                height={100}
+                priority
+              />
+            </Link>
+          </span>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
